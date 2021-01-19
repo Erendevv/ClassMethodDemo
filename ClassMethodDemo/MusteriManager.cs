d3842410ecc15e983a020a9b0b40349591340e2d
Eren Uğur <185022002@ogr.kastamonu.edu.tr>
2021-01-19 20:36:03 +0300

    Add project files.

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    public class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+
+            Console.WriteLine("Yeni Müşteri Eklendi :" + musteri.Ad);
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+
+            Console.WriteLine(musteri.Ad + " isimli müşteri silindi.");
+        }
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + " numaralı müşteri : " + musteri.Ad);
+            }
+        }
+
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Musteri Eren = new Musteri();
+            Eren.Id = 1;
+            Eren.Ad = "Eren";
+            Eren.Sehir = "Kastamonu";
+
+            Musteri Berk = new Musteri();
+            Berk.Id = 2;
+            Berk.Ad = "Berk";
+            Berk.Sehir = "Ordu";
+
+            Musteri[] musteriler = new Musteri[] { Eren, Berk };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(Berk);
+            musteriManager.MusteriEkle(Eren);
+            Console.WriteLine("**************************");
+            musteriManager.MusteriSil(Berk);
+            Console.WriteLine("**************************");
+            musteriManager.MusteriListele(musteriler);
+
+        }
+    }
+}
